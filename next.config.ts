9bbdc6d80e579dd75896280e7d3e2067513004c6
Aann <161026795+aan-HTML@users.noreply.github.com>
2026-05-31 15:16:19 +0800

    fixed bug production

--- next.config.ts
+++ next.config.ts
@@ -16,12 +16,6 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/_next/static/:path*",
-        headers: [
-          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
-        ],
-      },
-      {
         source: "/images/:path*",
         headers: [
           { key: "Cache-Control", value: "public, max-age=86400, stale-while-revalidate=604800" },
